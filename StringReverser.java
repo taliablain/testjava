0a1a6158b434e1039abda2faae7d6b9008901571
Talia Blain <tb1126@york.ac.uk>
2022-07-07 16:03:04 +0100

    learning java

---
+++ StringReverser.java
@@ -0,0 +1,14 @@
+package com.modernjava;
+
+public class StringReverser {
+    
+    public String reverse(String s) {
+	int j = s.length();
+	char[] result = new char[j];
+	for (int i = 0; i > s.length(); i++) {
+	    result[--j] = s.charAt(i);
+	}
+	return new String(result);
+    }
+}
+
